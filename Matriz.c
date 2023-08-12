4520fe55f4c3ad8cf3690e2b4835be49b06848e3
Kayque-Maciel <kayquemaciel000@gmail.com>
2023-08-11 21:33:56 -0300

    Finalizado

---
+++ Matriz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int linha, coluna;
+    int matriz[2][2];
+
+    for(linha = 0; coluna < 2; linha ++){
+
+        for(coluna = 0; coluna < 2; coluna ++){
+            printf("Digite  os valores da matriz para a linha %d, coluna %d: ", linha +1, coluna +1);
+            scanf("%d", &matriz[linha][coluna]);
+        }
+    }
+
+    for(linha = 0; linha <= 2; linha ++){
+
+        for(coluna = 0; coluna < 2; coluna ++)
+        printf("%d \t", matriz[linha][coluna]);
+    printf("Veja a sua matriz");
+    printf("\n\n");
+    }
+
+    system("pause");
+}
